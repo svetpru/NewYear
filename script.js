e68043a44725e523d8304c44302f932f526aa0d8
svetpru <sv.prud@gmail.com>
2020-12-13 15:00:46 +0300

    upset

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
 
+let music;
 
+$(document).ready(function () {
+
+    music = new Howl({
+       src: ["https://ruv.hotmo.org/get/cuts/a8/0c/a80cb0f4f75a49ac7dfc2696be0b6e98/48261645/Christmas_Music_Christmas_Songs_Jingle_Bells_Christmas_Hits_Collective_-_Jingle_Bell_Rock_b128f0d132.mp3"],
+       loop: true,
+       volume: 0.75
+    });
+});
+
+$("#player>img").click(function(e){
+    if(e.target.id != "isPlaying"){
+        music.play();
+        e.target.id = "isPlaying";
+    }
+});
