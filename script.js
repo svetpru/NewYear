5e20c9622d27eb034d7c935caee41789654f2c3c
svetpru <sv.prud@gmail.com>
2020-12-20 14:01:31 +0300

    upset

--- script.js
+++ script.js
@@ -5,15 +5,44 @@ let music;
 $(document).ready(function () {
 
     music = new Howl({
-       src: ["https://ruv.hotmo.org/get/cuts/a8/0c/a80cb0f4f75a49ac7dfc2696be0b6e98/48261645/Christmas_Music_Christmas_Songs_Jingle_Bells_Christmas_Hits_Collective_-_Jingle_Bell_Rock_b128f0d132.mp3"],
+       src: ["img/Audio.mpeg"],
        loop: true,
        volume: 0.75
     });
 });
 
-$("#player>img").click(function(e){
-    if(e.target.id != "isPlaying"){
-        music.play();
-        e.target.id = "isPlaying";
+$("#tree>img").click(function(e){
+    let name = e.target.id;
+
+    if(name == "toy1"){
+        music.play(0);
+    }
+    else if(name == "toy2"){
+        music.play(1);
+    }
+    else if(name == "toy3"){
+        music.play(2);
     }
 });
+
+
+//Интервал
+let timer = setInterval(function(){
+    //Время нового года
+    let newYear = new Date("1 January 2021 00:00:00");
+    //Нынешнее время
+    let now = new Date();
+    //Разность дат
+    let diff = newYear - now;
+
+    let seconds = Math.floor(diff/1000);
+    let minutes = Math.floor(seconds/60);
+    let hours = Math.floor(minutes/60);
+    let days = Math.floor(hours/24);
+
+    $("#seconds").text(seconds);
+    $("#minutes").text(minutes + ":");
+    $("#hours").text(hours + ":");
+    $("#days").text(days);
+
+}, 1000);
