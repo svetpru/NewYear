bb6027da736b3c112f2000c6b9d02d44c12c4827
svetpru <sv.prud@gmail.com>
2020-12-27 14:20:28 +0300

    upset

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ let music;
 $(document).ready(function () {
 
     //Интервал
-let timer = setInterval(function(){
-    //Время нового года
-    let newYear = new Date("1 January 2021 00:00:00");
-    //Нынешнее время
-    let now = new Date();
-    //Разность дат
-    let diff = newYear - now;
+    let timer = setInterval(function(){
+        //Время нового года
+        let newYear = new Date("1 January 2021 00:00:00");
+        //Нынешнее время
+        let now = new Date();
+        //Разность дат
+        let diff = newYear - now;
 
-    let days = Math.floor(diff/(24*60*60*1000));
-    let hours = Math.floor(diff%(24*60*60*1000) / (60*60*1000));
-    let minutes = Math.floor(diff%(60*60*1000) / (60*1000));
-    let seconds = Math.floor(diff%(60*1000) / 1000);
-    
+        let days = Math.floor(diff/(24*60*60*1000));
+        let hours = Math.floor(diff%(24*60*60*1000) / (60*60*1000));
+        let minutes = Math.floor(diff%(60*60*1000) / (60*1000));
+        let seconds = Math.floor(diff%(60*1000) / 1000);
 
-    $("#seconds").text(seconds);
-    $("#minutes").text(minutes + ":");
-    $("#hours").text(hours + ":");
-    $("#days").text(days);
 
-}, 1000);
+        $(".seconds").text(seconds);
+        $(".minutes").text(minutes + ":");
+        $(".hours").text(hours + ":");
+        $(".days").text(days);
+
+    }, 1000);
 
 
     music = new Howl({
@@ -56,7 +56,7 @@ $("#tree>img").click(function(e){
     }
     $("#treeStar").click(function() {
         music.stop();
-    });
+        });
 });
 
 //Анимация шляпы
@@ -77,3 +77,30 @@ setInterval(function(){
         $(".hand-r").removeClass("hand-rSwing");
     }, 2500);
 }, 3500);
+
+
+// Телефон
+//Муз дерево
+$("#treeMobile>img").click(function(e){
+
+    $("#treeMobile>img").addClass("sharking");
+
+    music.fade(1, 0, 1000, music);
+    let name = e.target.id;
+
+    if(name == "toy1"){
+        $(e.target).removeClass("sharking");
+        music.play();
+    }
+    else if(name == "toy2"){
+        $(e.target).removeClass("sharking");
+        music.play();
+    }
+    else if(name == "toy3"){
+        $(e.target).removeClass("sharking");
+        music.play();
+    }
+    $("#treeMobile>#treeStar").click(function() {
+        music.stop();
+        });
+});
