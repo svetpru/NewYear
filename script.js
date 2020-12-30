1d3f124a2cd8e4781e6dcb0647194da8fe49ca97
svetpru <sv.prud@gmail.com>
2020-12-30 14:27:06 +0300

    Upset

--- script.js
+++ script.js
@@ -104,3 +104,40 @@ $("#treeMobile>img").click(function(e){
         music.stop();
         });
 });
+
+
+// Отсчёт до нового года
+setInterval(function(){
+    // Время нового года
+    let newYear = new Date("1 January 2021 00:00:00"); 
+    // Нынешнее время
+    let now = new Date();
+    // Разность дат
+    let diff = newYear - now;
+
+    // Проверяем что таймер не истёк
+    if(diff > 0){
+        let days = Math.floor( diff/(24*60*60*1000) );
+        let hours = Math.floor( (diff%(24*60*60*1000)) / (60*60*1000) );
+        let minutes = Math.floor( (diff%(60*60*1000)) / (60*1000));
+        let seconds = Math.floor( (diff%(60*1000)) / 1000);
+
+        $(".seconds").text(seconds);
+        $(".minutes").text(minutes + " :");
+        $(".hours").text(hours + " :");
+        $(".days").text(days + " :");
+    }
+    else{
+        $("#countdown p, #countdownMobile p").text("Новый Год Наступил!")
+        $("#countdown, #countdownMobile").fadeOut(2000, function(){
+            $(this).remove();
+            $(".envlope-wrapper").fadeIn(1000).removeClass("d-none")
+        });
+    }
+}, 1000)
+
+// Открываем и закрываем открытку
+$(".envlope-wrapper").click(function (e) { 
+    $("#envelope").toggleClass("open");
+    $("#envelope").toggleClass("close");
+});
